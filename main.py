c32a96963eaaad70f7aad3c585ee32ace4166a07
Edwin Lee <leeed2001@gmail.com>
2025-06-06 14:55:47 -0500

    Error handling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from json import dumps, loads
 from pathlib import Path
-from shutil import copyfile, make_archive
+from shutil import copy, make_archive
 from subprocess import call
 from sys import argv
 
@@ -14,32 +14,35 @@ api = helper.get_api_instance()
 state = api.state_manager.new_state()
 
 current_dir = Path(__file__).parent
-json_convert_dir = current_dir / 'epjson_convert'
+idf_to_run = Path(helper.path_to_test_file('5ZoneAirCooled.idf'))
+
+# create a directory to handle IDF->JSON conversion and even JSON modifications
+json_convert_dir = current_dir / 'json_convert'
+json_convert_dir.mkdir()
+copied_idf = json_convert_dir / idf_to_run.name
+baseline_json = json_convert_dir / '5ZoneAirCooled.epJSON'
+modified_json = json_convert_dir / '5ZoneAirCooledMod.epJSON'
+print("Made JSON conversion directory and set up file paths.")
+
+# actually copy the IDF to the conversion directory and convert it to JSON
+copy(idf_to_run, json_convert_dir)
+call([convert_input_format, idf_to_run.name], shell=False, cwd=json_convert_dir)
+print("Converted IDF to baseline JSON file.")
+
+# now read in the JSON file that was created
+contents = loads(baseline_json.read_text())
+print("Read in JSON file from baseline IDF conversion.")
+
+# then modify it and write it out as the modified version
+contents['Timestep']['Timestep 1']['number_of_timesteps_per_hour'] = 1
+modified_json.write_text(dumps(contents, indent=4))
+print("Modified JSON and rewrote to new file")
+
+# run EnergyPlus with the baseline and modified JSON files
 output_dir_baseline = current_dir / 'output_baseline'
 output_dir_secondary = current_dir / 'output_secondary'
-
-# this simplifies when we just downselect a file and use the epjson version of it
-idf_to_run = Path(helper.path_to_test_file('5ZoneAirCooled.idf'))
-copied_idf = json_convert_dir / idf_to_run.name
-converted_json = json_convert_dir / '5ZoneAirCooled.epJSON'
-copyfile(idf_to_run, copied_idf)
-call([convert_input_format, idf_to_run.name], shell=False, cwd=json_convert_dir)
-print("Converted IDF to JSON format.")
-
-return_value = api.runtime.run_energyplus(state, ['-D', converted_json, '-d', output_dir_baseline])
-assert(return_value == 0, "EnergyPlus run failed with original JSON input file.")
-print("Ran EnergyPlus with baseline JSON input file.")
-
-contents = loads(converted_json.read_text())
-print("Read JSON contents to prepare for modifications.")
-
-contents['Timestep']['Timestep 1']['number_of_timesteps_per_hour'] = 1
-new_json_file = output_dir_secondary / '5ZoneAirCooledMod.epJSON'
-new_json_file.write_text(dumps(contents, indent=4))
-print("Rewrote JSON file with modified timestep.")
-
-return_value = api.runtime.run_energyplus(state, ['-D', new_json_file, '-d', output_dir_secondary])
-assert(return_value == 0, "EnergyPlus run failed with modified JSON input file.")
+api.runtime.run_energyplus(state, ['-D', baseline_json, '-d', output_dir_baseline])
+api.runtime.run_energyplus(state, ['-D', modified_json, '-d', output_dir_secondary])
 print("Ran EnergyPlus with baseline JSON input file.")
 
 make_archive('baseline', 'zip', root_dir=output_dir_baseline)
